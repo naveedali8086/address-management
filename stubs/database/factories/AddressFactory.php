0832d56d774433a1094f95e61dd343b3eab4cb13
Naveed Ali <naveedali8086@gmail.com>
2024-01-29 22:26:14 +0500

    Wip - fixed namespace issue

--- stubs/database/factories/AddressFactory.php
+++ stubs/database/factories/AddressFactory.php
@@ -21,34 +21,34 @@ class AddressFactory extends Factory
     public function definition(): array
     {
         $countries = Country::select('id')->pluck('id')->toArray();
-        $selectedCountryId = \Database\Factories\fake()->randomElement($countries);
+        $selectedCountryId = fake()->randomElement($countries);
 
         $regions = Region::select('id')->where('country_id', $selectedCountryId)->pluck('id')->toArray();
-        $selectedRegionId = \Database\Factories\fake()->randomElement($regions);
+        $selectedRegionId = fake()->randomElement($regions);
 
         $cities = City::select('id')->where('region_id', $selectedRegionId)->pluck('id')->toArray();
-        $selectedCityId = \Database\Factories\fake()->randomElement($cities);
+        $selectedCityId = fake()->randomElement($cities);
 
         $definition = [
-            'line_1' => \Database\Factories\fake()->streetAddress(),
-            'line_2' => \Database\Factories\fake()->randomElement([\Database\Factories\fake()->streetAddress(), null]),
-            'postal_code' => \Database\Factories\fake()->randomElement([\Database\Factories\fake()->postcode(), null]),
+            'line_1' => fake()->streetAddress(),
+            'line_2' => fake()->randomElement([fake()->streetAddress(), null]),
+            'postal_code' => fake()->randomElement([fake()->postcode(), null]),
             'country_id' => $selectedCountryId,
             'region_id' => $selectedRegionId,
             'city_id' => $selectedCityId,
-            'landmark' => \Database\Factories\fake()->randomElement([\Database\Factories\fake()->word(), null]),
-            'lat' => \Database\Factories\fake()->randomElement([\Database\Factories\fake()->latitude(), null]),
-            'long' => \Database\Factories\fake()->randomElement([\Database\Factories\fake()->longitude(), null]),
-            'belongs_to' => \Database\Factories\fake()->randomElement(AddressBelongsTo::cases()),
-            'applicable_for_shipping' => \Database\Factories\fake()->randomElement([0, 1])
+            'landmark' => fake()->randomElement([fake()->word(), null]),
+            'lat' => fake()->randomElement([fake()->latitude(), null]),
+            'long' => fake()->randomElement([fake()->longitude(), null]),
+            'belongs_to' => fake()->randomElement(AddressBelongsTo::cases()),
+            'applicable_for_shipping' => fake()->randomElement([0, 1])
         ];
 
         // set longitude if latitude is set
         if (isset($defination['lat']) && !isset($defination['long'])) {
-            $definition['long'] = \Database\Factories\fake()->longitude();
+            $definition['long'] = fake()->longitude();
         } else if (isset($defination['long']) && !isset($defination['lat'])) {
             // set latitude if longitude is set
-            $definition['lat'] = \Database\Factories\fake()->latitude();
+            $definition['lat'] = fake()->latitude();
         }
 
         return $definition;
